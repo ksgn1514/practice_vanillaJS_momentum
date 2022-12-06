e93218b238919ed4b9adbc64c2a7918aa5cef45a
kim.sg <ksgn1514@gmail.com>
2022-12-06 22:19:24 +0900

    form submission

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const loginButton = loginForm.querySelector("button");
 
 function onLoginButtonClick() {
     console.dir(loginInput);
-    console.log(loginInput.value);
+    //console.log(loginInput.value);
+    
+    //Form Submit으로 아래 자동으로 html선에서 가능
+    // const userName = loginInput.value;
+    // if (userName==="") {
+    //     alert("Please write your name");
+    // } else if (userName.length > 15) {
+    //     alert("Your name is too long")
+    // }
+
 }
 
 loginButton.addEventListener("click", onLoginButtonClick)
