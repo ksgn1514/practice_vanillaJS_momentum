7280bda2cbfdb2c5db4f38b4abc45edc436b2fc2
Kim.sg <ksgn1514@gmail.com>
2023-11-06 17:16:10 +0900

    css modifying (1)

--- js/background.js
+++ js/background.js
@@ -1,7 +1,9 @@
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImage}`;
+// document.body.appendChild(bgImage);
 
-document.body.appendChild(bgImage);
+const body = document.body;
+body.background = `img/${chosenImage}`;
--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,10 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const infoBox = document.querySelector("#info-box");
+const todoBox = document.querySelector("#todo-box");
+const quotes = document.querySelector("#quotes");
+
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -9,14 +13,17 @@ function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY,username);
-    loginForm.classList.add(HIDDEN_CLASSNAME);
     paintGreetings(username);
 }
 
 function paintGreetings(username){
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `${username}'s Todo List`;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    todoBox.classList.remove(HIDDEN_CLASSNAME);
+    infoBox.classList.remove(HIDDEN_CLASSNAME);
+    quotes.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -27,3 +34,57 @@ if(savedUsername === null){
 } else {
     paintGreetings(savedUsername);
 }
+
+
+
+// quotes.js integral
+const quotesList = [
+    {
+        quote:"Be yourself; everyone else is already taken.",
+        author:"Oscar Wilde",
+    },
+    {
+        quote:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+        author:"Marilyn Monroe",
+    },
+    {
+        quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author:"Albert Einstein",
+    },
+    {
+        quote:"So many books, so little time.",
+        author:"Frank Zappa",
+    },
+    {
+        quote:"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        author:"Bernard M. Baruch",
+    },
+    {
+        quote:"You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        author:"Dr. Seuss",
+    },
+    {
+        quote:"You only live once, but if you do it right, once is enough.",
+        author:"Mae West",
+    },
+    {
+        quote:"Be the change that you wish to see in the world.",
+        author:"Mahatma Gandhi",
+    },
+    {
+        quote:"In three words I can sum up everything I've learned about life: it goes on.",
+        author:"Robert Frost",
+    },
+    {
+        quote:"If you tell the truth, you don't have to remember anything.",
+        author:"Mark Twain",
+    },
+]
+
+
+const quote = quotes.querySelector("span:first-child")
+const author = quotes.querySelector("span:last-child");
+
+const todaysQuote= quotesList[Math.floor(Math.random() * quotesList.length)];
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
