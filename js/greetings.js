b91ac91524ecbea272b38ba5a4e25c24074c7ea8
kim.sg <ksgn1514@gmail.com>
2023-11-09 17:41:30 +0900

    css modify Last , Add Quick link

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const infoBox = document.querySelector("#info-box");
+const linkBox = document.querySelector("#links-box");
 const todoBox = document.querySelector("#todo-box");
 const quotes = document.querySelector("#quotes");
 
@@ -12,11 +13,11 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username);
+    localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
 
-function paintGreetings(username){
+function paintGreetings(username) {
     //greeting.innerText = "Hello " + username;
     greeting.innerText = `${username}'s Todo List`;
     loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -24,11 +25,12 @@ function paintGreetings(username){
     todoBox.classList.remove(HIDDEN_CLASSNAME);
     infoBox.classList.remove(HIDDEN_CLASSNAME);
     quotes.classList.remove(HIDDEN_CLASSNAME);
+    linkBox.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
+if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
@@ -40,44 +42,44 @@ if(savedUsername === null){
 // quotes.js integral
 const quotesList = [
     {
-        quote:"Be yourself; everyone else is already taken.",
-        author:"Oscar Wilde",
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde",
     },
     {
-        quote:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-        author:"Marilyn Monroe",
+        quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+        author: "Marilyn Monroe",
     },
     {
-        quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-        author:"Albert Einstein",
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein",
     },
     {
-        quote:"So many books, so little time.",
-        author:"Frank Zappa",
+        quote: "So many books, so little time.",
+        author: "Frank Zappa",
     },
     {
-        quote:"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-        author:"Bernard M. Baruch",
+        quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        author: "Bernard M. Baruch",
     },
     {
-        quote:"You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-        author:"Dr. Seuss",
+        quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        author: "Dr. Seuss",
     },
     {
-        quote:"You only live once, but if you do it right, once is enough.",
-        author:"Mae West",
+        quote: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West",
     },
     {
-        quote:"Be the change that you wish to see in the world.",
-        author:"Mahatma Gandhi",
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi",
     },
     {
-        quote:"In three words I can sum up everything I've learned about life: it goes on.",
-        author:"Robert Frost",
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost",
     },
     {
-        quote:"If you tell the truth, you don't have to remember anything.",
-        author:"Mark Twain",
+        quote: "If you tell the truth, you don't have to remember anything.",
+        author: "Mark Twain",
     },
 ]
 
@@ -85,6 +87,6 @@ const quotesList = [
 const quote = quotes.querySelector(".quote")
 const author = quotes.querySelector(".author");
 
-const todaysQuote= quotesList[Math.floor(Math.random() * quotesList.length)];
+const todaysQuote = quotesList[Math.floor(Math.random() * quotesList.length)];
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
